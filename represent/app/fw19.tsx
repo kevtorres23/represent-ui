1a2400d6567f5bc6557ce9bde6e31081bbcab165
Kevin Torres <kevintu236@gmail.com>
2025-06-16 11:34:58 -0600

    Interfaz FW19

--- represent/app/fw19.tsx
+++ represent/app/fw19.tsx
@@ -1,12 +1,75 @@
-import { View, Text } from 'react-native';
-import ProductCard from '@/components/ProductCard';
+import { View, Text, Image, StyleSheet, SafeAreaView, ScrollView, RefreshControl, Platform } from "react-native";
+import React, { useState } from "react";
+import ProductCard from "@/components/ProductCard";
 
-function FW19 () {
-    return(
-        <View className=''>
-            <Text>Hola</Text>
-        </View>
-    )
+type productProps = {
+    imageSrc: any,
+    name: string,
+    price: number,
 }
+
+function FW19(props: productProps) {
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        setTimeout(() => {
+            setRefreshing(false);
+        }, 1000)
+    }, []);
+
+    return (
+        <SafeAreaView style={styles.safe} className='bg-slate-50 items-center justify-center'>
+            <ScrollView contentContainerStyle={styles.scrollView}>
+                <View className="flex-1 items-center justify-center w-screen gap-16 pt-6">
+                    <View className="flex-row gap-5">
+                        <ProductCard imageSrc={require('../assets/images/hoodie1.png')} name="THE ETERNAL THRILL HOODIE" price={50} />
+                        <ProductCard imageSrc={require('../assets/images/tshirt1.png')} name="HONDA T-SHIRT BLACK STYLE" price={60} />
+                    </View>
+                    <View className="flex-row gap-5">
+                        <ProductCard imageSrc={require('../assets/images/hoodie2.png')} name="GREEN BASIC STYLE HOODIE" price={80} />
+                        <ProductCard imageSrc={require('../assets/images/tshirt2.png')} name="REPRESENT ORIGINALS T-SHIRT" price={45} />
+                    </View>
+                    <View className="flex-row gap-5">
+                        <ProductCard imageSrc={require('../assets/images/hoodie1.png')} name="THE ETERNAL THRILL HOODIE" price={50} />
+                        <ProductCard imageSrc={require('../assets/images/tshirt1.png')} name="HONDA T-SHIRT BLACK STYLE" price={60} />
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView >
+    );
+}
+
+const styles = StyleSheet.create({
+    productName: {
+        fontFamily: "FugazOne",
+    },
+
+    safe: {
+        flex: 1,
+        paddingTop: Platform.OS === "android" ? 0 : 20,
+        paddingBottom: 20,
+    },
+
+    scrollView: {
+        alignItems: "center",
+        justifyContent: "center",
+        paddingHorizontal: 0,
+        paddingVertical: 20,
+        width: "auto",
+    },
+
+    titles: {
+        fontFamily: "FugazOne",
+    },
+
+    image1: {
+        backgroundColor: 'rgba(0,0,0, 0.15)',
+    },
+
+    image2: {
+        backgroundColor: 'rgba(0,0,0, 0.30)',
+    }
+})
 
 export default FW19;
--- represent/components/ProductCard.tsx
+++ represent/components/ProductCard.tsx
@@ -1,17 +1,27 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 type productProps = {
-    imageSrc: string,
+    imageSrc: any,
     name: string,
     price: number,
-} 
+}
 
 function ProductCard(props: productProps) {
-    return(
-        <View>
-            
+    return (
+        <View className="flex-col gap-2 max-h-60 items-center justify-center max-w-44">
+            <View className="bg-slate-200 rounded-xl py-4">
+                <Image source={props.imageSrc} className="max-w-full max-h-44" />
+            </View>
+            <Text style={styles.productName} className="text-center text-base">{props.name}</Text>
+            <Text className="text-center text-base text-slate-500 font-semibold mt-[-5px]">${props.price} USD</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    productName: {
+        fontFamily: "FugazOne",
+    }
+})
 
 export default ProductCard;
